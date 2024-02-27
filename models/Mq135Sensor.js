815f1be0ac43df1d5852a3dd7bd62bad5e229409
Anderson Rodrigues <and.rt@hotmail.com>
2024-02-27 14:59:12 -0300

    Structuring Mq135 Sensor Model

--- models/Mq135Sensor.js
+++ models/Mq135Sensor.js
@@ -1,7 +1,37 @@
 const status = require("http-status");
 
 class Mq135Sensor{
+    constructor(){
+        this.table = "mq135readings";
+    }
 
+    async findAll(){
+        try{
+            return await knex.select().table(this.table);
+        }catch(err){
+            throw err;
+        }
+    }
+
+    async create(reading){
+        try{
+            return await knex.insert({
+                isFire: reading.value,
+                date: reading.date
+            }).table(this.table);
+        } catch(err){
+            throw err;
+        }
+    }
+
+    async findById(id){
+        try{
+            const reading = await knex.select().where({id}).first().table(this.table);
+            return reading;
+        } catch(err){
+            throw err;
+        }
+    }
 }
 
 module.exports = new Mq135Sensor();
