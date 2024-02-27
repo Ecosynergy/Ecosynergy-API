432b31c1cacf3a009c4ae45e4950149c7cd6d0f0
Anderson Rodrigues <and.rt@hotmail.com>
2024-02-27 08:39:55 -0300

    Structuring Fire Sensor Model

--- models/FireSensor.js
+++ models/FireSensor.js
@@ -1,7 +1,38 @@
+const knex = require("../database/connection");
 const status = require("http-status");
 
 class FireSensor{
+    constructor(){
+        this.table = "firereadings"
+    }
+    async findAll(){
+        try{
+            return await knex.select().table(this.table);
+        }catch(err){
+            throw err;
+        }
+    }
 
+    async create(reading){
+        try{
+            return await knex.insert({
+                isFire: reading.isFire,
+                date: reading.date
+            }).table(this.table);
+        } catch(err){
+            throw err;
+        }
+    }
+
+    async findById(id){
+        try{
+            const reading = await knex.select().where({id}).first().table(this.table);
+
+            return reading;
+        } catch(err){
+            throw err;
+        }
+    }
 }
 
 module.exports = new FireSensor();
