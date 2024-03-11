e166071cc64add3a22879add610f6246a9a72b12
Anderson Rodrigues <and.rt@hotmail.com>
2024-03-11 09:35:26 -0300

    Initial Commit

---
+++ EcosynergyAPI/src/main/java/app/ecosynergy/api/EcosynergyApiApplication.java
@@ -0,0 +1,13 @@
+package app.ecosynergy.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcosynergyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcosynergyApiApplication.class, args);
+    }
+
+}
---
+++ EcosynergyAPI/src/test/java/app/ecosynergy/api/EcosynergyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package app.ecosynergy.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcosynergyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
