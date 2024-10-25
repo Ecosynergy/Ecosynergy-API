4e54a4579e3b65da63c84d979d97a75313ffe0c1
Rafael Evangelista <gueerae@gmail.com>
2024-10-25 11:37:30 -0300

    Update TokenController.java

--- EcosynergyAPI/src/main/java/app/ecosynergy/api/controllers/TokenController.java
+++ EcosynergyAPI/src/main/java/app/ecosynergy/api/controllers/TokenController.java
@@ -16,7 +16,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
-@RequestMapping("/api/tokens/v1")
+@RequestMapping("/api/token/v1")
 @Tag(name = "Token Management", description = "Operations related to FCM token management.")
 public class TokenController {
 
