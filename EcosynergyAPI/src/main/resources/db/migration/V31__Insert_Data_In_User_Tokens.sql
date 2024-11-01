2028556f808f5547c6c024726c98a1bf9734f5c6
Anderson Rodrigues <and.rt@hotmail.com>
2024-10-31 22:11:08 -0300

    Update Notifications Structure

--- EcosynergyAPI/src/main/resources/db/migration/V31__Insert_Data_In_User_Tokens.sql
+++ EcosynergyAPI/src/main/resources/db/migration/V31__Insert_Data_In_User_Tokens.sql
@@ -1,1 +1,1 @@
-INSERT INTO `ecosynergy`.`user_tokens` (`id`, `token`, `device_type`, `is_active`, `user_id`) VALUES ('1', 'csTZKYcmRu-BXd261NiIGw:APA91bGGf_WozjsYIKdnjZdRSBjmSR0XCm5UYTHf8J_fxNDIebxR09HUj4mC19hSF3W3G7-AHRGHkIV1UseR__9EcWGEgb-wsif2cDeI6LG3oCFuLbiqnag', 'android', '1', '1');
+INSERT INTO user_tokens VALUES ('1', 'csTZKYcmRu-BXd261NiIGw:APA91bGGf_WozjsYIKdnjZdRSBjmSR0XCm5UYTHf8J_fxNDIebxR09HUj4mC19hSF3W3G7-AHRGHkIV1UseR__9EcWGEgb-wsif2cDeI6LG3oCFuLbiqnag', 'android', CURRENT_TIMESTAMP, '1', '1');
